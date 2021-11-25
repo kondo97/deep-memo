650fd2d05a3d52881c29b404b270b58a5d1543c8
kondo97 <kondo9523@gmail.com>
2021-11-25 16:13:43 +0900

    一覧画面作成

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,14 +1,21 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { Paper, Grid } from "@mui/material";
+import { Paper, Grid, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { formatDate } from "pages/hooks/formatDate";
+import { turnCate } from "pages/hooks/turnCate"
+import styles from "styles/Home.module.css"
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+import removeMd from 'remove-markdown';
+
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: "center",
   color: theme.palette.text.secondary,
-  height: 150,
+  height: 160,
   marginTop: 30,
 }));
 
@@ -21,16 +28,26 @@ export type PostProps = {
   title: string;
 };
 
-const Post: React.FC<{ post: PostProps }> = ({post}) => {
+const dummy = "この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーでaa"
+
+const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   return (
     <>
-      <Item elevation={3}>
-        {post.title}
-        {post.content}
-        {post.createdAt}
+      <Item elevation={3} className={styles.parent}>
+        <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant="subtitle1" align="left" sx={{fontWeight: 600}}>{turnCate(dummy, 25)}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="subtitle2" align="left">{turnCate(removeMd(dummy), 30)}</Typography>
+        </Grid>
+        <Grid item xs={12} className={styles.childBottomRight}>
+          <Typography variant="subtitle2">{formatDate(post.createdAt)}</Typography>
+        </Grid>
+        </Grid>
       </Item>
     </>
   );
-}
+};
 
-export default Post
+export default Post;
