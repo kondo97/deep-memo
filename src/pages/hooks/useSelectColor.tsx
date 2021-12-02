09a7f5747a84c3623f4406bbc3a030b3ad2c4cb3
kondo97 <kondo9523@gmail.com>
2021-12-02 17:03:01 +0900

    ビルド修正

--- src/pages/hooks/useSelectColor.tsx
+++ src/pages/hooks/useSelectColor.tsx
@@ -4,10 +4,10 @@ const useSelectColor = (props: string) => {
   let postColor
   if(props === 'a') return postColor = theme.palette.postThemeA
   if(props === 'b') return postColor = theme.palette.postThemeB
-  if(props=== 'C') return postColor = theme.palette.postThemeC
+  if(props === 'C') return postColor = theme.palette.postThemeC
   if(props === 'd') return postColor = theme.palette.postThemeD
   if(props === 'e') return postColor = theme.palette.postThemeE
-  return theme.palette.primary
+  return postColor = theme.palette.primary
 }
 
 export default useSelectColor
