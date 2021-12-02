b779457b0069400bb8da1295ec4ffa86365e8407
kondo97 <kondo9523@gmail.com>
2021-12-02 17:10:46 +0900

    ビルドトライ

--- src/pages/hooks/useSelectColor.tsx
+++ src/pages/hooks/useSelectColor.tsx
@@ -1,11 +1,11 @@
 import theme from "src/color/Theme"
 
 const useSelectColor = (props: string) => {
-  if(props === 'a') return theme.palette.postThemeA
-  if(props === 'b') return theme.palette.postThemeB
-  if(props === 'C') return theme.palette.postThemeC
-  if(props === 'd') return theme.palette.postThemeD
-  if(props === 'e') return theme.palette.postThemeE
+  // if(props === 'a') return theme.palette.postThemeA
+  // if(props === 'b') return theme.palette.postThemeB
+  // if(props === 'C') return theme.palette.postThemeC
+  // if(props === 'd') return theme.palette.postThemeD
+  // if(props === 'e') return theme.palette.postThemeE
   return theme.palette.primary
 }
 
